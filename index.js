1faf83082b270f797d30bc8a854ecdc3a03366ee
alamgir hossain <alamgir555hossain@gmail.com>
2020-03-21 20:01:14 -0400

    all

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
 const fs = require('fs');
-const http = require('http')
+const http = require('http');
+const url = require('url');
 
-// const txt = fs.readFileSync('./txt/append.txt', 'utf-8');
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
 
-// const textOut = `This is something at i added. ${txt}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('file added');
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    return output;
+    
+}
 
-// fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
-//     console.log(data1);
-//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
-//         console.log(data2);
-//         fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
-//             console.log(data3);
-//             fs.writeFile('.txt/final.txt', `${data2}\n${data3}`, 'utf-8', (error) => {
-//                 console.log('final compeled');
-//             });
-//         })
-//     })
-// });
+const tempOverView = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
-// console.log('started');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
 
 const server = http.createServer((req, res) => {
-    res.end('heloo');
+    const {query, pathname} = url.parse(req.url, true);
+
+
+    if(pathname === '/' || pathname === '/overview'){
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const cardHTML = dataObject.map(el => replaceTemplate(tempCard, el));
+        const output = tempOverView.replace('{%PRODUCT_CARD%}', cardHTML);
+
+        res.end(output);
+
+    }else if(pathname === '/product'){
+        res.writeHead(200, {'Content-type': 'text/html'});
+       const product = dataObject[query.id];
+       const output = replaceTemplate(tempProduct, product);
+
+        res.end(output);
+
+    }else if(pathname === '/api'){
+      
+        res.writeHead(200, {'Content-type': 'application/json'});
+        console.log(err)
+        res.end(data);
+    }
+    else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>this page not found</h1>');
+    }
+    
 });
 
 server.listen(8000, '127.0.0.1', () => {
